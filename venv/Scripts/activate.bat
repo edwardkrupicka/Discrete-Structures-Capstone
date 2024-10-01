00301c5abc2024d020d8e44ce48c6326aa0960a1
edwardkrupicka <edwardkrupicka@gmail.com>
2024-10-01 13:54:14 -0600

    created requirements and readme

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Visual Studio Code\DS Capstone\venv
+set VIRTUAL_ENV=C:\Users\edwar\OneDrive\Documents\VSCode\Discrete-Structures-Capstone\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
